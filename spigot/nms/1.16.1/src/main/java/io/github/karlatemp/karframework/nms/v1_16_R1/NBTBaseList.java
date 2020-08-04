e0466380e1e21dd54b2220c0e9812335a3d1d3a6
Karlatemp <karlatemp@vip.qq.com>
2020-08-04 21:08:53 +0800

    NBT Framework - 1.16.1

---
+++ spigot/nms/1.16.1/src/main/java/io/github/karlatemp/karframework/nms/v1_16_R1/NBTBaseList.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * @create 2020/08/04 19:16:18
+ *
+ * kar-framework/kar-framework.spigot-nms-1_16_1.main/NBTBaseList.java
+ */
+
+package io.github.karlatemp.karframework.nms.v1_16_R1;
+
+import io.github.karlatemp.karframework.opennbt.ITag;
+import net.minecraft.server.v1_16_R1.NBTList;
+
+public abstract class NBTBaseList<
+        T extends NBTList<?>
+        > extends NBTBaseWrapper<T> {
+    public NBTBaseList(T base) {
+        super(base);
+    }
+
+    public boolean set(int index, ITag value) {
+        return base.a(index, ((NBTBaseWrapper<?>) value).base);
+    }
+
+    public boolean add(int index, ITag value) {
+        return base.b(index, ((NBTBaseWrapper<?>) value).base);
+    }
+}
---
+++ spigot/nms/1.16.1/src/main/java/io/github/karlatemp/karframework/nms/v1_16_R1/NBTByteArray.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * @create 2020/08/04 19:11:34
+ *
+ * kar-framework/kar-framework.spigot-nms-1_16_1.main/NBTByteArray.java
+ */
+
+package io.github.karlatemp.karframework.nms.v1_16_R1;
+
+import io.github.karlatemp.karframework.opennbt.ITagByteArray;
+import net.minecraft.server.v1_16_R1.NBTTagByte;
+import net.minecraft.server.v1_16_R1.NBTTagByteArray;
+import org.jetbrains.annotations.NotNull;
+
+public class NBTByteArray
+        extends NBTBaseList<NBTTagByteArray>
+        implements ITagByteArray {
+    public NBTByteArray(NBTTagByteArray base) {
+        super(base);
+    }
+
+    @Override
+    public byte get(int index) {
+        return base.get(index).asByte();
+    }
+
+    @Override
+    public byte remove(int index) {
+        return base.remove(index).asByte();
+    }
+
+    @Override
+    public @NotNull byte[] getValues() {
+        return base.getBytes();
+    }
+
+    @Override
+    public void clear() {
+        base.clear();
+    }
+
+    @Override
+    public byte set(int index, byte value) {
+        return base.set(index, NBTTagByte.a(value)).asByte();
+    }
+
+    @Override
+    public void add(int index, byte value) {
+        base.add(index, NBTTagByte.a(value));
+    }
+
+    @Override
+    public int size() {
+        return base.size();
+    }
+
+    @Override
+    public @NotNull ITagByteArray clone() {
+        return new NBTByteArray((NBTTagByteArray) base.clone());
+    }
+}
